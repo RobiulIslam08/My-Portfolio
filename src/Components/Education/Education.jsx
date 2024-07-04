a235f18504be1109f82e77e5e70690cd3bac4c61
Robiul Islam <robiulislamrobi0874@gmail.com>
2024-07-04 18:50:08 +0600

    project card design

--- src/Components/Education/Education.jsx
+++ src/Components/Education/Education.jsx
@@ -15,12 +15,25 @@ const Education = () => {
 				</div>
 
 				<div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
+					
 					<div
 
-						className={`shadow-md  hover:scale-105 duration-500 py-2 rounded-lg border-lime-500 border-r border-l`}
+						className={`shadow-md text-left pl-4 space-y-1  hover:scale-105 duration-500 py-2 rounded-lg border-lime-500 border-r border-l`}
 					>
-						<p>Robiu</p>
-						<p className="mt-4 ">Dedekjlkds</p>
+						<p className="font-bold text-2xl text-gray-300">Coputer Science and Technology</p>
+						<p className="text-gray-400">Diploma in Engineering</p>
+						<p className="text-gray-400">Sherpur Polytechnic Institute</p>
+						<p  className="text-gray-400">2021 - 2022</p>
+					
+					</div>
+					<div
+
+						className={`shadow-md text-left pl-4 space-y-1  hover:scale-105 duration-500 py-2 rounded-lg border-lime-500 border-r border-l`}
+					>
+						<p className="font-bold text-2xl text-gray-300">Secondary School  Certificate</p>
+						<p className="text-gray-400">Rehana Idris Model Academy</p>
+						<p className="text-gray-400">Studied science subjects, including physics, chemistry, biology, and mathematics.</p>
+						<p  className="text-gray-400">2020 - 2021</p>
 					</div>
 				</div>
 			</div>
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,74 +1,345 @@
+// // import React from 'react'
 
-import arrayDestruct from "../../assets/projectsImage/arrayDestruct.jpg";
-import installNode from "../../assets/projectsImage/installNode.jpg";
-import navbar from "../../assets/projectsImage/navbar.jpg";
-import reactParallax from "../../assets/projectsImage/reactParallax.jpg";
-import reactSmooth from "../../assets/projectsImage/reactSmooth.jpg";
-import reactWeather from "../../assets/projectsImage/reactWeather.jpg";
+// // import javascript from "../../assets/javascript.png";
+// // import reactImage from "../../assets/react.png";
+// // import node from "../../assets/node.png";
+// // import ProjectsCard from './ProjectsCard';
+// // import { BsGithub } from 'react-icons/bs';
+// // import { FaGlobe } from 'react-icons/fa';
+
+// // const Projects = () => {
+// //   return (
+// //     <section
+// //       id="projects"
+// //       className="w-full pl-4 py-20 border-b-[1px] border-b-black"
+// //     >
+// //       <div>
+// //         <p className="text-4xl  font-bold border-b-4 border-gray-500 p-2 inline">
+// //           Projects
+// //         </p>
+// //         <p className="py-6 text-gray-400">These are the technologies I've worked with</p>
+// //       </div>
+// //       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
+       
+// //         <div className="w-full bg-[#35463552] p-4 xl:px-12 h-auto  xl:py-10 rounded-lg shadow-shadowOne flex flex-col bg-gradient-to-r from-bodyColor to-[#202327] group hover:bg-gradient-to-b hover:from-gray-900 hover:gray-900 transition-colors duration-1000">
+// //           <div className="w-full h-[80%] overflow-hidden rounded-lg">
+// //             <img
+// //               className="w-full h-60 object-cover group-hover:scale-110 duration-300 cursor-pointer"
+// //               src={node}
+// //               alt="src"
+// //             />
+// //           </div>
+// //           <div className="w-full mt-5 flex flex-col  gap-6">
+// //             <div>
+
+// //              <div className='flex justify-between'>
+// //              <h3 className="text-base uppercase font-normal">
+// //                 Toursid Guide Website
+// //               </h3>
+// //               <button className="btn btn-xs">
+
+                
+// //                 Live Link
+// //               </button>
+// //              </div>
+// //               {/* <div className="flex gap-2">
+// //               <span className="text-lg w-10 h-10 rounded-full bg-black inline-flex justify-center items-center text-gray-400 hover:text-designColor duration-300 cursor-pointer">
+// //                 <BsGithub />
+// //               </span>
+// //               <span className="text-lg w-10 h-10 rounded-full bg-black inline-flex justify-center items-center text-gray-400 hover:text-designColor duration-300 cursor-pointer">
+// //                 <FaGlobe />
+// //               </span>
+// //             </div> */}
+
+// //               <p className="text-sm tracking-wide mt-3 hover:text-gray-100 duration-300">
+// //                 Your go-to online resource for exploring Bangladesh, offering tailored features for tourists, guides, and admins.
+// //               </p>
+// //               <div className='flex justify-between mt-1'>
+// //                 <button className="btn btn-xs">
+
+// //                   <BsGithub></BsGithub>
+// //                   Client-Github
+// //                 </button>
+// //                 <button className="btn btn-xs">
+
+// //                   <BsGithub></BsGithub>
+// //                   Client-Github
+// //                 </button>
+
+// //               </div>
+// //             </div>
+// //           </div>
+// //         </div>
+// //       </div>
+// //     </section>
+// //   );
+// // }
+
+// // export default Projects
+
+
+
+import React from 'react';
+import javascript from "../../assets/javascript.png";
+import tourista from "../../assets/projectsImage/screencapture-tourista-81113-web-app-2024-07-04-18_05_29.png"
+import reactImage from "../../assets/react.png";
+import node from "../../assets/node.png";
+import { BsGithub } from 'react-icons/bs';
 
 const Projects = () => {
-  const portfolios = [
-    {
-      id: 1,
-      src: arrayDestruct,
-    },
-    {
-      id: 2,
-      src: reactParallax,
-    },
-    {
-      id: 3,
-      src: navbar,
-    },
-    {
-      id: 4,
-      src: reactSmooth,
-    },
-    {
-      id: 5,
-      src: installNode,
-    },
-    {
-      id: 6,
-      src: reactWeather,
-    },
-  ];
-
   return (
-    <div
-      name="portfolio"
-      className="w-full pt-28 lg:pt-32 text-white "
+    <section
+      id="projects"
+      className="w-full pl-4 py-20 border-b-[1px] border-b-black"
     >
-      <div className="p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
-            Portfolio
-          </p>
-          <p className="py-6">Check out some of my work right here</p>
-        </div>
-
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src }) => (
-            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
-              <img
-                src={src}
-                alt=""
-                className="rounded-md duration-200 hover:scale-105"
-              />
-              <div className="flex items-center justify-center">
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                  Demo
+      <div>
+        <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
+          Projects
+        </p>
+        <p className="py-6 text-gray-400">These are the technologies I've worked with</p>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
+        <div className="w-full bg-[#3546352a] p-4 xl:px-12 h-auto xl:py-10 rounded-lg shadow-shadowOne flex flex-col bg-gradient-to-r from-bodyColor to-[#202327] group hover:bg-gradient-to-b hover:from-gray-900 hover:gray-900 transition-colors duration-1000 relative overflow-hidden">
+          <div className="w-full h-60 overflow-hidden rounded-lg">
+            <img
+              className="w-full h-full overflow-y-auto  group-hover:scale-110 duration-300 cursor-pointer"
+              src={tourista}
+              alt="src"
+            />
+          </div>
+          <div className="w-full mt-5 flex flex-col gap-6">
+            <div>
+              <div className='flex justify-between items-center'>
+                <h3 className="text-base uppercase  font-bold">
+                  Toursit Guide Website
+                </h3>
+                <button className="text-xs bg-gray-700 px-2 py-1 rounded hover:bg-gray-600 transition duration-300">
+                  
+                  <a href="https://tourista-81113.web.app/"  target='_blank'>Live Link</a>
                 </button>
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                  Code
+              </div>
+              <p className="text-sm tracking-wide mt-3 hover:text-gray-100 duration-300">
+                Your go-to online resource for exploring Bangladesh, offering tailored features for tourists, guides, and admins.
+              </p>
+              <div className='flex justify-between mt-1'>
+                <button className="text-xs bg-indigo-800 text-white px-2 py-1 rounded hover:bg-indigo-600 transition duration-300 flex items-center gap-1">
+                  <BsGithub />
+                  <a href="https://github.com/RobiulIslam08/a12-tourista-client" target='_blank'>
+                  Client-Github
+                  </a>
+                </button>
+                <button className="text-xs bg-indigo-800 px-2 py-1 text-white rounded hover:bg-indigo-600 transition duration-300 flex items-center gap-1">
+                  <BsGithub /> <a href="https://github.com/RobiulIslam08/a12-tourista-server" target='_blank'> Server-Github</a> 
                 </button>
               </div>
             </div>
-          ))}
+          </div>
+          <div className="absolute flex flex-wrap text-center text-gray-300 text-xs font-bold opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            <span className="mx-2">HTML</span>
+            <span className='border-r '></span>
+            <span className="mx-2">CSS</span>
+            <span className='border-r'></span>
+            <span className="mx-2">React</span>
+            <span className='border-r'></span>
+            <span className="mx-2">Mongodb</span>
+            <span className='border-r'></span>
+            <span className="mx-2">Enpress</span>
+            <span className='border-r'></span>
+            <span className="mx-2">Node</span>
+            <span className='border-r'></span>
+            <span className="mx-2">Firebase</span>
+           
+           
+          </div>
+        </div>
+        <div className="w-full bg-[#3546352a] p-4 xl:px-12 h-auto xl:py-10 rounded-lg shadow-shadowOne flex flex-col bg-gradient-to-r from-bodyColor to-[#202327] group hover:bg-gradient-to-b hover:from-gray-900 hover:gray-900 transition-colors duration-1000 relative overflow-hidden">
+          <div className="w-full h-60 overflow-hidden rounded-lg">
+            <img
+              className="w-full h-full overflow-y-auto  group-hover:scale-110 duration-300 cursor-pointer"
+              src={tourista}
+              alt="src"
+            />
+          </div>
+          <div className="w-full mt-5 flex flex-col gap-6">
+            <div>
+              <div className='flex justify-between items-center'>
+                <h3 className="text-base uppercase  font-bold">
+                  Toursit Guide Website
+                </h3>
+                <button className="text-xs bg-gray-700 px-2 py-1 rounded hover:bg-gray-600 transition duration-300">
+                  
+                  <a href="https://tourista-81113.web.app/"  target='_blank'>Live Link</a>
+                </button>
+              </div>
+              <p className="text-sm tracking-wide mt-3 hover:text-gray-100 duration-300">
+                Your go-to online resource for exploring Bangladesh, offering tailored features for tourists, guides, and admins.
+              </p>
+              <div className='flex justify-between mt-1'>
+                <button className="text-xs bg-indigo-800 text-white px-2 py-1 rounded hover:bg-indigo-600 transition duration-300 flex items-center gap-1">
+                  <BsGithub />
+                  <a href="https://github.com/RobiulIslam08/a12-tourista-client" target='_blank'>
+                  Client-Github
+                  </a>
+                </button>
+                <button className="text-xs bg-indigo-800 px-2 py-1 text-white rounded hover:bg-indigo-600 transition duration-300 flex items-center gap-1">
+                  <BsGithub /> <a href="https://github.com/RobiulIslam08/a12-tourista-server" target='_blank'> Server-Github</a> 
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="absolute flex flex-wrap text-center text-gray-300 text-xs font-bold opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            <span className="mx-2">HTML</span>
+            <span className='border-r '></span>
+            <span className="mx-2">CSS</span>
+            <span className='border-r'></span>
+            <span className="mx-2">React</span>
+            <span className='border-r'></span>
+            <span className="mx-2">Mongodb</span>
+            <span className='border-r'></span>
+            <span className="mx-2">Enpress</span>
+            <span className='border-r'></span>
+            <span className="mx-2">Node</span>
+            <span className='border-r'></span>
+            <span className="mx-2">Firebase</span>
+           
+           
+          </div>
+        </div>
+        <div className="w-full bg-[#3546352a] p-4 xl:px-12 h-auto xl:py-10 rounded-lg shadow-shadowOne flex flex-col bg-gradient-to-r from-bodyColor to-[#202327] group hover:bg-gradient-to-b hover:from-gray-900 hover:gray-900 transition-colors duration-1000 relative overflow-hidden">
+          <div className="w-full h-60 overflow-hidden rounded-lg">
+            <img
+              className="w-full h-full overflow-y-auto  group-hover:scale-110 duration-300 cursor-pointer"
+              src={tourista}
+              alt="src"
+            />
+          </div>
+          <div className="w-full mt-5 flex flex-col gap-6">
+            <div>
+              <div className='flex justify-between items-center'>
+                <h3 className="text-base uppercase  font-bold">
+                  Toursit Guide Website
+                </h3>
+                <button className="text-xs bg-gray-700 px-2 py-1 rounded hover:bg-gray-600 transition duration-300">
+                  
+                  <a href="https://tourista-81113.web.app/"  target='_blank'>Live Link</a>
+                </button>
+              </div>
+              <p className="text-sm tracking-wide mt-3 hover:text-gray-100 duration-300">
+                Your go-to online resource for exploring Bangladesh, offering tailored features for tourists, guides, and admins.
+              </p>
+              <div className='flex justify-between mt-1'>
+                <button className="text-xs bg-indigo-800 text-white px-2 py-1 rounded hover:bg-indigo-600 transition duration-300 flex items-center gap-1">
+                  <BsGithub />
+                  <a href="https://github.com/RobiulIslam08/a12-tourista-client" target='_blank'>
+                  Client-Github
+                  </a>
+                </button>
+                <button className="text-xs bg-indigo-800 px-2 py-1 text-white rounded hover:bg-indigo-600 transition duration-300 flex items-center gap-1">
+                  <BsGithub /> <a href="https://github.com/RobiulIslam08/a12-tourista-server" target='_blank'> Server-Github</a> 
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="absolute flex flex-wrap text-center text-gray-300 text-xs font-bold opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            <span className="mx-2">HTML</span>
+            <span className='border-r '></span>
+            <span className="mx-2">CSS</span>
+            <span className='border-r'></span>
+            <span className="mx-2">React</span>
+            <span className='border-r'></span>
+            <span className="mx-2">Mongodb</span>
+            <span className='border-r'></span>
+            <span className="mx-2">Enpress</span>
+            <span className='border-r'></span>
+            <span className="mx-2">Node</span>
+            <span className='border-r'></span>
+            <span className="mx-2">Firebase</span>
+           
+           
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
-};
+}
 
 export default Projects;
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import javascript from "../../assets/javascript.png";
+// import tourista from "../../assets/projectsImage/screencapture-tourista-81113-web-app-2024-07-04-18_05_29.png";
+// import reactImage from "../../assets/react.png";
+// import node from "../../assets/node.png";
+// import { BsGithub } from 'react-icons/bs';
+
+// const Projects = () => {
+//   return (
+//     <section
+//       id="projects"
+//       className="w-full pl-4 py-20 border-b-[1px] border-b-black"
+//     >
+//       <div>
+//         <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
+//           Projects
+//         </p>
+//         <p className="py-6 text-gray-400">These are the technologies I've worked with</p>
+//       </div>
+//       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
+//         <div className="w-full bg-[#3546352d] p-4 xl:px-12 h-auto xl:py-10 rounded-lg shadow-shadowOne flex flex-col bg-gradient-to-r from-bodyColor to-[#202327] group hover:bg-gradient-to-b hover:from-gray-900 hover:gray-900 transition-colors duration-1000 relative overflow-hidden">
+//           <div className="w-full h-60 overflow-hidden rounded-lg">
+//             <img
+//               className="w-full h-full overflow-auto group-hover:scale-110 duration-300 cursor-pointer"
+//               src={tourista}
+//               alt="src"
+//             />
+//           </div>
+//           <div className="w-full mt-5 flex flex-col gap-6">
+//             <div>
+//               <div className='flex justify-between items-center'>
+//                 <h3 className="text-base uppercase font-normal">
+//                   Toursid Guide Website
+//                 </h3>
+//                 <button className="text-xs bg-gray-700 px-2 py-1 rounded hover:bg-gray-600 transition duration-300">
+//                   Live Link
+//                 </button>
+//               </div>
+//               <p className="text-sm tracking-wide mt-3 hover:text-gray-100 duration-300">
+//                 Your go-to online resource for exploring Bangladesh, offering tailored features for tourists, guides, and admins.
+//               </p>
+//               <div className='flex justify-between mt-1'>
+//                 <button className="text-xs bg-indigo-800 text-white px-2 py-1 rounded hover:bg-indigo-600 transition duration-300 flex items-center gap-1">
+//                   <BsGithub /> Client-Github
+//                 </button>
+//                 <button className="text-xs bg-indigo-800 px-2 py-1 text-white rounded hover:bg-indigo-600 transition duration-300 flex items-center gap-1">
+//                   <BsGithub /> Server-Github
+//                 </button>
+//               </div>
+//             </div>
+//           </div>
+//           <div className="absolute inset-0 flex items-center justify-center  text-white text-lg font-bold opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+//             <div className="flex flex-wrap justify-center items-center text-gray-400">
+//               <span className="mx-2">HTML</span>
+//               <span className="mx-2">CSS</span>
+//               <span className="mx-2">JavaScript</span>
+//               <span className="mx-2">HTML</span>
+//               <span className="mx-2">CSS</span>
+//               <span className="mx-2">JavaScript</span>
+//               <span className="mx-2">HTML</span>
+//               <span className="mx-2">CSS</span>
+//               <span className="mx-2">JavaScript</span>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// }
+
+// export default Projects;
